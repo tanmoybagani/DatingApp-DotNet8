c6a7893fd88e964ca61a19d7ca9efba4552d2a29
tanmoybagani <tanmoybagani@outlook.com>
2024-08-28 11:10:19 +0530

    Created API Skeleton

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UsersController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _dbContext.Users.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<AppUser>> GetUserById([FromRoute] int id)
+        {
+            var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return user;
+        }
+    }
+}
---
+++ API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
